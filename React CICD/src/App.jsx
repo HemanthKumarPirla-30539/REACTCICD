07ddb3122eafd2d8e0b6a16a58af6dfc14e7d496
HemanthKumarPirla-30539 <2300030539xcseh@gmail.com>
2025-07-21 10:28:31 +0530

    react application

---
+++ React CICD/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+//the above three statements not used so it is showing like this
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+<label className='mystyle'>KL university</label>
+  
+  
+   </>
+  )
+}
+
+export default App
